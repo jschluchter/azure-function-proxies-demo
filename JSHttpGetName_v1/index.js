6c2458dd4ac2ab39b7ac1afae372d99ce41150eb
Joey Schluchter <jschluchter@gmail.com>
2017-04-24 00:20:08 -0400

    moved directories

---
+++ JSHttpGetName_v1/index.js
@@ -0,0 +1,8 @@
+﻿module.exports = function(context, req) {
+    context.log("Retrieved records:", "500");
+    context.res = {
+        status: 200,
+        body: { "name": req.name }
+    };
+    context.done();
+};
