6d76796dfe61a628d2a8e68b7ba2da14d21871f7
Joey Schluchter <jschluchter@gmail.com>
2017-04-24 01:31:51 -0400

    add request

--- JSHttpGetName_v1/index.js
+++ JSHttpGetName_v1/index.js
@@ -1,11 +1,37 @@
-﻿module.exports = function(context, req) {
+﻿var rp = require('request-promise');
 
-    var inputName = context.bindingData.name;
+module.exports = function(context, req) {
 
-    context.log("Retrieved records:", "500");
-    context.res = {
-        status: 200,
-        body: { name: inputName }
+    var inputId = context.bindingData.id;
+    context.log('Looking for user %s', inputId);
+
+    var options = {
+        uri: 'https://jsonplaceholder.typicode.com/users/' + inputId,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
     };
-    context.done();
+
+    var response = {
+        body: {},
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    rp(options)
+        .then(function(user) {
+            context.log('Found user %s', user);
+            response.body = user;
+        })
+        .catch(function(err) {
+            context.log('Danger Will Robinson! Error=%s', err);
+            response.status = 500;
+        })
+        .finally(function() {
+            context.res = response;
+            context.done();
+        });
+
+
 };
